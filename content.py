b0121ff7014085d061d0093c35113620fff44813
V. Avalos <vavalosla@gmail.com>
2021-04-28 22:04:31 -0700

    Working. Revisit rm_files()

--- content.py
+++ content.py
@@ -1,32 +1,33 @@
 import os
 
 username = ""
-userfile = ""
 paths = []
-
-def valid_input(prompt, choices):
-    while True:
-        answer = input(f"{prompt} {choices}").lower()
-        if answer in choices:
-            return answer
-        print('Please choose a valid option.')
-
-
-def user(userfile):
-    userfile = valid_input("Please select your profile name:\n", f"\n{paths}\n")
-    print(userfile)
 
 
 def find_temp():  # Finds the users temp folder. 
     os.chdir("C:\\Users\\")
+    print(os.listdir())
     start = os.listdir(".")
     for path in start:
         paths.append(path)
+        try:
+            os.chdir(f"C:\\Users\\{path}\\AppData\\Local\\Temp")
+        except PermissionError:
+            paths.remove(path)
+            print('-----------\n[ErrorNoPerms: '+ path + ']\n==')
+        except FileNotFoundError:
+            paths.remove(path)
+            print('-----------\n[ErrorNotFound: '+ path + ']\n==')
+    rm_files()
     
-    user(userfile)
-    # os.chdir(f"C:\\Users\\{userfile}\\AppData\\Local\\Temp")
+def rm_files():
+    files = os.listdir()
+    for file in files:
+        print(file+"\nWILL BE REMOVED\n----------")
 
+    # print(paths)
 
 
 
 find_temp()
+# print(os.listdir())
