a9b28926f1a80953d288b5bd9238c43ecc619ef7
V. Avalos <vavalosla@gmail.com>
2021-05-08 11:21:38 -0700

    fix: Fixed merge mistakes

--- content.py
+++ content.py
@@ -26,35 +26,26 @@ def find_temp():  # Finds the users temp folder
 def rm_files():  # Removes files in Temp/ directory
     files = os.listdir()
     for file in files:
-<<<<<<< HEAD
         try:
             if '.tmp' or '.txt' in file:
                 os.remove(file)
                 print(file)
             else:
-                shutil.rmtree(file)
+                shutil.rmtree(file)  # Deltes folders w/ or w/o files
             deleted.append(file)
+
         except PermissionError:
             remaining.append(file)
     print(f"{', '.join(remaining)}\n^^^ Could not be deleted...")
+
     if len(deleted) == 0:
         print("\n>> Nothing was removed...\n")
     else:
         print(f"{deleted}\n * DELETED *\n---")
-
-||||||| b0121ff
-        print(file+"\nWILL BE REMOVED\n----------")
-=======
         try:
             os.remove(file)
         except PermissionError:
             print(file+ " [PermissionError]")
 
->>>>>>> e5343aff5bb2781534ddc62a81d9c2d79c03f418
-
-    # print(paths)
-
-
 
 find_temp()
-# print(os.listdir())
